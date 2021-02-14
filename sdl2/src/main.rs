74e4d9d548551af82887cdb8d82e818b56cb3e15
Filipe Rainho <filipenrainho@gmail.com>
2021-02-14 11:12:17 +0100

    Add structopt and support for cli args
    Now the emulator needs the rom path and you can optionally define the hz
    you need depending on the rom

--- sdl2/src/main.rs
+++ sdl2/src/main.rs
@@ -1,5 +1,5 @@
-use number_generator::RandomNumberGenerator;
-use std::{error::Error, thread, time::Duration};
+use std::{error::Error, path::PathBuf, thread, time::Duration};
+use structopt::StructOpt;
 
 mod audio;
 mod graphics;
@@ -11,11 +11,24 @@ use audio::SdlAudio;
 use chip8_core::{Chip8, State};
 use graphics::SdlGraphics;
 use keyboard::SdlKeyboard;
+use number_generator::RandomNumberGenerator;
 use rom_loader::RomLoader;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let sdl_context = sdl2::init()?;
+#[derive(StructOpt, Debug)]
+#[structopt(name = "chip8-sdl")]
+struct CliArgs {
+    #[structopt(long = "rom", short = "r")]
+    rom: PathBuf,
+    #[structopt(long = "hertz", short = "h", default_value = "500")]
+    hertz: u32,
+}
 
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli_args = CliArgs::from_args();
+    let rom_data = RomLoader::load_rom(&cli_args.rom)?;
+    let sleep_time = 1000 / cli_args.hertz;
+
+    let sdl_context = sdl2::init()?;
     let sdl_audio = SdlAudio::new(&sdl_context)?;
     let sdl_graphics = SdlGraphics::new(&sdl_context)?;
     let sdl_keyboard = SdlKeyboard::new(&sdl_context)?;
@@ -26,7 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Box::new(sdl_keyboard),
         Box::new(sdl_graphics),
     );
-    let rom_data = RomLoader::load_rom("Space Invaders.ch8")?;
+
     chip8.load_program(rom_data)?;
 
     'main: loop {
@@ -34,8 +47,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             break 'main;
         };
 
-        // 500hz
-        thread::sleep(Duration::from_millis(2));
+        thread::sleep(Duration::from_millis(sleep_time.into()));
     }
 
     Ok(())
--- sdl2/src/rom_loader.rs
+++ sdl2/src/rom_loader.rs
@@ -3,12 +3,10 @@ use std::{error::Error, fs, path::PathBuf};
 pub struct RomLoader;
 
 impl RomLoader {
-    const ROMS_FOLDER: &'static str = "./roms";
-
-    pub fn load_rom(rom_name: &str) -> Result<Vec<u8>, Box<dyn Error>> {
-        let mut rom_path = PathBuf::from(RomLoader::ROMS_FOLDER);
-        rom_path.push(rom_name);
-
-        Ok(fs::read(rom_path)?)
+    pub fn load_rom<P>(rom_path: P) -> Result<Vec<u8>, Box<dyn Error>>
+    where
+        P: Into<PathBuf>,
+    {
+        Ok(fs::read(rom_path.into())?)
     }
 }
